a3adadb379053c733912d2e9b502a0c8054a722b
Craftidore <elliot@madbear.com>
2024-01-26 09:38:23 -0600

    I can't read, apparently.

--- config/scripts/steamworkaround.sh
+++ config/scripts/steamworkaround.sh
@@ -3,15 +3,15 @@
 set -eou pipefail
 
 if [ ! -f "/tmp/steam-workaround-run" ]; then
+    touch /tmp/steam-workaround-run
     sed -i '0,/enabled=0/s//enabled=1/' /etc/yum.repos.d/rpmfusion-nonfree-steam.repo
     sed -i '0,/enabled=1/s//enabled=0/' /etc/yum.repos.d/rpmfusion-nonfree.repo
     sed -i '0,/enabled=1/s//enabled=0/' /etc/yum.repos.d/rpmfusion-nonfree-updates.repo
     sed -i '0,/enabled=1/s//enabled=0/' /etc/yum.repos.d/fedora-updates.repo
-    rm /tmp/steam-workaround-run
 else
-    touch /tmp/steam-workaround-run
     sed -i '0,/enabled=1/s//enabled=0/' /etc/yum.repos.d/rpmfusion-nonfree-steam.repo
     sed -i '0,/enabled=0/s//enabled=1/' /etc/yum.repos.d/rpmfusion-nonfree.repo
     sed -i '0,/enabled=0/s//enabled=1/' /etc/yum.repos.d/rpmfusion-nonfree-updates.repo
     sed -i '0,/enabled=0/s//enabled=1/' /etc/yum.repos.d/fedora-updates.repo
+    rm /tmp/steam-workaround-run
 fi
