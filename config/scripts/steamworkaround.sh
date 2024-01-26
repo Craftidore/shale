5e046c571f3fd630c36e11434f88f27fc38e4de1
Craftidore <elliot@madbear.com>
2024-01-26 07:40:43 -0600

    Trying to install steam again

---
+++ config/scripts/steamworkaround.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+set -eou pipefail
+
+if [ ! -f /tmp/steam-workaround-run ]; do
+    sed -i '0,/enabled=0/s//enabled=1/' /etc/yum.repos.d/rpmfusion-nonfree-steam.repo
+    sed -i '0,/enabled=1/s//enabled=0/' /etc/yum.repos.d/rpmfusion-nonfree.repo
+    sed -i '0,/enabled=1/s//enabled=0/' /etc/yum.repos.d/rpmfusion-nonfree-updates.repo
+    sed -i '0,/enabled=1/s//enabled=0/' /etc/yum.repos.d/fedora-updates.repo
+    rm /tmp/steam-workaround-run
+else
+    touch /tmp/steam-workaround-run
+    sed -i '0,/enabled=1/s//enabled=0/' /etc/yum.repos.d/rpmfusion-nonfree-steam.repo
+    sed -i '0,/enabled=0/s//enabled=1/' /etc/yum.repos.d/rpmfusion-nonfree.repo
+    sed -i '0,/enabled=0/s//enabled=1/' /etc/yum.repos.d/rpmfusion-nonfree-updates.repo
+    sed -i '0,/enabled=0/s//enabled=1/' /etc/yum.repos.d/fedora-updates.repo
+fi
