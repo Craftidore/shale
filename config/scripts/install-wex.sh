3a9354cf3344cd3c6fcdc568415d35cff465f0cd
Craftidore <elliot@madbear.com>
2024-06-04 12:30:20 -0500

    remove wex-runner

--- config/scripts/install-wex.sh
+++ config/scripts/install-wex.sh
@@ -3,8 +3,5 @@
 set -oue pipefail
 
 curl -sS https://raw.githubusercontent.com/madx/wex/main/wex > /tmp/wex
-curl -sS https://raw.githubusercontent.com/Craftidore/wex/main/wex-runner > /tmp/wex-runner
 chmod +x /tmp/wex
-chmod +x /tmp/wex-runner
 cp /tmp/wex /usr/bin/wex
-cp /tmp/wex-runner /usr/bin/wex-runner
