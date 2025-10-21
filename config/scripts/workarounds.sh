c68e94bee1c02444a26025585f9e0305f4c5a40d
Craftidore <elliot@madbear.com>
2025-10-21 11:07:36 -0500

    Comment out workarounds.sh

--- config/scripts/workarounds.sh
+++ config/scripts/workarounds.sh
@@ -3,5 +3,5 @@
 set -oeux pipefail
 
 # alternatives cannot create symlinks on its own during a container build
-echo "Linking ld"
-ln -sf /usr/bin/ld.bfd /etc/alternatives/ld && ln -sf /etc/alternatives/ld /usr/bin/ld
+# echo "Linking ld"
+# ln -sf /usr/bin/ld.bfd /etc/alternatives/ld && ln -sf /etc/alternatives/ld /usr/bin/ld
