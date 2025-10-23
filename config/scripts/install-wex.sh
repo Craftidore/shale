7b1ea27695d7ca462a0d1b5d0d55ee4c5caed047
Craftidore <elliot@madbear.com>
2025-10-23 17:29:38 -0500

    fix duplicate rpm-fusion install requests

--- config/scripts/install-wex.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-
-set -oue pipefail
-
-curl -sS https://raw.githubusercontent.com/madx/wex/main/wex > /tmp/wex
-chmod +x /tmp/wex
-cp /tmp/wex /usr/bin/wex
--- config/scripts/services.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -oue pipefail
-
-# Your code goes here.
-echo "Enabling keyd"
-systemctl enable keyd
-systemctl enable sshd
